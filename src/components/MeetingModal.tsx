de010ff762592d63f0c14d9719530d1732d3a835
vighnesh <vighneshzagade4@gmail.com>
2024-09-01 23:44:48 +0530

    random-commit

---
+++ src/components/MeetingModal.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  handleClick?: () => void;
+  buttonText?: string;
+  image?: string;
+  buttonIcon?: string;
+}
+
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  className,
+  buttonText,
+  handleClick,
+  children,
+  image,
+  buttonIcon,
+}: MeetingModalProps) => {
+  return <div>MeetingModal</div>;
+};
+
+export default MeetingModal;
